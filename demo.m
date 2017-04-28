3e0a4fb0216f5afb02411502e81881c5f659d0d3
Ritwick Chaudhry <14D070063@iitb.ac.in>
2017-04-29 00:33:46 +0530

    Displaying Emotions in Demo now

--- demo.m
+++ demo.m
@@ -1,3 +1,11 @@
+emotion1 = imread('EmotionDisplay/Angry.jpg');
+emotion6 = imread('EmotionDisplay/Sad.png');
+emotion5 = imread('EmotionDisplay/Happy.jpg');
+emotion3 = imread('EmotionDisplay/Disgust.jpeg');
+emotion4 = imread('EmotionDisplay/Fear.png');
+emotion7 = imread('EmotionDisplay/Surprised.jpeg');
+emotion2 = imread('EmotionDisplay/Contempt.png');
+
 test_dir = '..\demo';
 % test_dir = uigetdir();
 files = dir(test_dir);
@@ -45,4 +53,28 @@ testData = eigenVecs'*(testData - repmat(meanTemplate,[1 numTest]));
 %LDA(trainingData, trainingLabels, testData)
 %SVM(trainingData, trainingLabels, testData)
 labels2 = LDA_HOG_SVM(trainingData, trainingLabels, testData, trainingData2, testData2)
+
+for i=1:numTest
+    figure;
+    
+    if labels2(i) == 1
+        emotionPic = emotion1
+    elseif labels2(i) == 2
+        emotionPic = emotion2
+    elseif labels2(i) == 3
+        emotionPic = emotion3
+    elseif labels2(i) == 4
+        emotionPic = emotion4
+    elseif labels2(i) == 5
+        emotionPic = emotion5
+    elseif labels2(i) == 6
+        emotionPic = emotion6
+    else
+        emotionPic = emotion7
+    end
+    
+    emotionPic = imresize(emotionPic,[64 64]);   
+    imshow([reshape(testData(i),[64 64]) emotionPic]);
+end
+    
 % testModel(labels2)
